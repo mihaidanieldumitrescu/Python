c29361c1af78e3baf648b28140c477a1382ea1e7
Mihai Dumitrecu <dumitr.mihai@gmail.com>
2017-09-30 18:14:52 +0300

    Wrote the method of represeting elements from entries object to HTML

--- WriteHtmlOutput.py
+++ WriteHtmlOutput.py
@@ -7,38 +7,70 @@ from pprint import pprint
 class WriteHtmlOutput:
 
 	def __init__(self, entry):
-	        self.htmlOutput = HTML()
-		self.printMe = entry
-		pprint (self.printMe.retReference)
-		self.writeOutput()
 
+		self.buffer = ""	        
+		self.htmlOutput = HTML()
+		self.printMe = entry
+		self.totalSpentMonth = {}
+		self.totalSpentCategory = {}
+		self.bufferMainTable()
+		self.writeOutput()
+		
+
+	def bufferMainTable(self):
+		monthsArr  = [ "August", "September", "October", "November", "December", "January" , "February" , "March" , "April", "May" , "June" , "July" ]
+
+		table = self.htmlOutput.table()
+		index = 0
+
+		for currMonth in monthsArr:
+		#for each defined period aug - july
+			
+			index += 1
+			tdBefore = index 
+			tdAfter = 12 - index
+ 			self.totalSpentMonth[currMonth] = 0
+			entries = self.printMe.dictCurrentYear
+
+			headerRow = table.tr ( klass="background-color: lightblue")
+			headerRow.td ( currMonth  ,klass="background-color: lightblue")			
+			
+			for period in ["liquidation", "advance" ]:
+				hasEntries = len ( self.printMe.getEntriesFor( period, currMonth ))
+				if not hasEntries:
+					continue
+
+				headerRow.td(period)
+				secondRow = table.tr
+
+				for entryNew in self.printMe.getEntriesFor( period, currMonth ):
+					thirdRow = table.tr
+					for time in range(tdBefore):
+						thirdRow.td() #empty div before input
+					thirdRow.td  ( entryNew.description)
+					thirdRow.td  ( entryNew.value ) 
+ 					#self.totalSpentMonth[currMonth] +=  entryNew.value
+					self.totalSpentCategory[entryNew.label] = 0
+					
+				for time in range(tdAfter):
+					headerRow.td() #empty div after input
+				headerRow = table.tr ( klass="background-color: lightblue")
+				headerRow.td()
+
+		self.buffer +=   str (self.htmlOutput ) 
+
+	def bufferHeader(self):
+		self.buffer += ""
+
+	def bufferFooter(self):
+		self.buffer += ""	
+
+	
 	def writeOutput(self):
 		output = open ( "budgetStatistics.html", "w")
-       		current = EntryNew()
-		table = self.htmlOutput.table()
-		for entry in self.printMe.retReference():
-		    row = table.tr
-		    if current.period != entry.period:
-		        current.period = entry.period
-		        row.td( entry.period )
-		    else:
-		        row.td()
-		        
-		    if current.month != entry.month:
-		        current.month = entry.month
-		        row.td( entry.month )
-		    else:
-		        row.td()
-		        
-		    if current.year != entry.year:
-		        current.year = entry.year
-		        row.td( entry.year )
-		    else:
-		        row.td()
-		    row.td( entry.description )
-		    row.td( entry.value )
-		    row.td( entry.label )
-
-		output.write ( str (self.htmlOutput ) )
+		output.write ( self.buffer )
 		output.close
-
+		print  "by Category\n\n"
+		pprint (self.totalSpentCategory )
+		print  "\nby Month\n\n"
+		pprint ( self.totalSpentMonth )
