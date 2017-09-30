faf16c267cbd54ee976e02e241a4894fc5574940
Mihai Dumitrecu <dumitr.mihai@gmail.com>
2017-09-30 18:16:12 +0300

    Minor modifications

--- Main.py
+++ Main.py
@@ -4,19 +4,12 @@
 from Entries import Entries
 from WriteHtmlOutput import WriteHtmlOutput
 
-print "Main ran ..."
+print "Main ran ...\n"
 
 debug = 0
 
 lastYear = Entries()
-#lastYear.printValues()
+
 if debug == 1:
 	lastYear.loadDebugValues()
 writer = WriteHtmlOutput(lastYear)
-#lastYear.writeOutputHTML()
-
-months  = [ "August", "September", "November", "December" ]
-period  = ["liquidation", "advance" ]
-
-lastYear.printValuesDict()
-#lastYear.writeGPchart()
