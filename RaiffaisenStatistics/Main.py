33e25c65910792187a94c617e0ff483fb29b6d5f
Mihai Dumitrecu <dumitr.mihai@gmail.com>
2017-10-28 11:52:42 +0300

    integrated xlrd module into Main.py

--- RaiffaisenStatistics/Main.py
+++ RaiffaisenStatistics/Main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+import glob
+
+import xlrd
 from includes.Entries import Entries
 from includes.WriteHtmlOutput import WriteHtmlOutput
 
@@ -7,9 +10,17 @@ print "Main ran ...\n"
 
 debug = 0
 
+files = glob.glob("./input/*xls")
+
+for f in files:
+	print "trying to open " + f 
+	book = xlrd.open_workbook( f )
+	sh = book.sheet_by_index(0)
+	print("{0} {1} {2}".format(sh.name, sh.nrows, sh.ncols))
+	for rx in range(sh.nrows):
+    		print(sh.row(rx))	
 
 
 if debug == 1:
 	lastYear.loadDebugValues()
-writer = WriteHtmlOutput( Entries("statistici2016.csv") )
-2
+#writer = WriteHtmlOutput( Entries("statistici2016.csv") )
