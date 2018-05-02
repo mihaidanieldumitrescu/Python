910d7d5ac2cdfc1d4c8bbf604a2c91b65db0b4e3
Mihai-Daniel Dumitrescu <mihaidanieldumitrescu@github.com>
2018-05-02 23:41:02 +0300

    Changes to be committed: 	modified:   includes/RaiffaisenStatement.py
     Small improvements

--- RaiffaisenStatistics/includes/RaiffaisenStatement.py
+++ RaiffaisenStatistics/includes/RaiffaisenStatement.py
@@ -9,12 +9,12 @@ class Statement(object):
 
 	def __init__(self):
 		self.pp = pprint.PrettyPrinter( indent=4)
-		self.statements ={
+		self.data ={
 								"headers" : {
 												"Data generare extras": None,
 												"Numar extras" : None,
 												"Nume client" : None,
-												"Adresa" : None,
+												"Adresa client" : None,
 												"Numar client" : None,
 												"Cod BIC Raiffeisen Bank" : None,
 												"Unitate Bancara" : None,
@@ -29,8 +29,10 @@ class Statement(object):
 												"Rulaj creditor" : 0,
 												"Sold final" : 0
 											},
-								"operations" : [	
-												{		
+								"operations" : []
+		}
+
+		self.example = { 													
 													"Data inregistrare"  : "" ,
 													"Data tranzactiei" : "" ,
 													"Suma debit" : "" ,
@@ -43,43 +45,40 @@ class Statement(object):
 													"Denumire Banca ordonator/ beneficiar" : "" ,
 													"Nr. cont in/din care se efectueaza tranzactiile" : "" ,
 													"Descrierea tranzactiei" : "" 
-												}
-											]
-							}
-
+												
+						}
+			
 	def readStatement(self):
 		pass
 	
 	def loadStatement(self, filename ):
 		files = filename
-		if len ( files ) == 0:
-			print "No files found in folder \n"
-
-		print "trying to open " + filename
-		book = xlrd.open_workbook( filename )
+		fullPathToFile = os.path.join ( os.environ['OneDrive'], "PythonData" ,"extrasDeCont", filename )
+		print "trying to open " + fullPathToFile
+		book = xlrd.open_workbook( fullPathToFile )
 		sh = book.sheet_by_index(0)
 
 		for rx in range(sh.nrows):
 			index = 0
 			currRow = sh.row(rx)
 			if rx == 0:
-				self.statements['headers']['Data generare extras'] = currRow[1].value
+				self.data['headers']['Data generare extras'] = currRow[1].value
 			elif rx == 1:
-				self.statements['headers']['Numar extras'] = currRow[1].value
+				self.data['headers']['Numar extras'] = currRow[1].value
 			elif rx == 5:
-				self.statements['headers']['Nume client'] = currRow[1].value
+				self.data['headers']['Nume client'] = currRow[1].value
 			elif rx == 6:
-				self.statements['headers']['Adresa client'] = currRow[1].value			
+				self.data['headers']['Adresa client'] = currRow[1].value			
 			elif rx == 14:
 
-				self.statements['rulaj']['Sold initial'] =   currRow[0].value 
-				self.statements['rulaj']['Rulaj debitor'] =  currRow[1].value 					
-				self.statements['rulaj']['Rulaj creditor'] =   currRow[2].value  
-				self.statements['rulaj']['Sold final'] =  currRow[3].value 
+				self.data['rulaj']['Sold initial'] =   currRow[0].value 
+				self.data['rulaj']['Rulaj debitor'] =  currRow[1].value 					
+				self.data['rulaj']['Rulaj creditor'] =   currRow[2].value  
+				self.data['rulaj']['Sold final'] =  currRow[3].value 
 			elif rx > 17:
 				
-
-				self.statements['operations'].append ( {
+				if len ( currRow[1].value.split('/') ) == 3:
+					self.data['operations'].append ( {
 					
 									"Data inregistrare"  : currRow[0].value ,
 									"Data tranzactiei" : currRow[1].value ,
@@ -89,14 +88,11 @@ class Statement(object):
 									"Descrierea tranzactiei" : currRow[11].value
 								} )
 
-		self.pp.pprint (self.statements)
-
-	def popStatement(self):
-		pass
+		#self.pp.pprint (self.data)
 
 if __name__ == "__main__":
 	testObj = Statement()
 	if len ( sys.argv ) > 1:
-		testObj.loadStatement(os.path.join ( os.environ['OneDrive'], "PythonData" ,"extrasDeCont", sys.argv[1] ))
+		testObj.loadStatement(  sys.argv[1] )
 	else:
-		print "Please specify a excel file"
+		print "Please specify a excel file found in 'Onedrive\PythonData\extrasDeCont' "
