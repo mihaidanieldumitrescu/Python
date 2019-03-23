371788fb8db8f079072d3145fa48e69ede0e6fad
Mihai-Daniel Dumitrescu <dumitr.mihai@gmail.com>
2019-03-23 22:04:02 +0200

    Changes to be committed: 	modified:   RaiffaisenStatistics/mySQL_interface/GenerateSQLfile.py
     - Working version.

--- RaiffaisenStatistics/mySQL_interface/GenerateSQLfile.py
+++ RaiffaisenStatistics/mySQL_interface/GenerateSQLfile.py
@@ -1,6 +1,11 @@
 # BRANCH IS REDESIGN
 
 from main.EntryNew import EntryNew
+from mySQL_interface.credentials import *
+import pymysql
+
+connection = pymysql.connect(host=HOSTNAME, user=USERNAME, password=PASSWORD, db=DATABASE,
+                             cursorclass=pymysql.cursors.DictCursor)
 
 
 class GenerateSQLfile:
@@ -16,5 +21,21 @@ class GenerateSQLfile:
             self.entries.append(entry)
         else:
             raise TypeError('Only EntryNew objects are supported for this method')
+
+    def export_contents(self):
+        print("Exporting data to mySQL database ...")
+        global connection
+
+        with connection.cursor() as cursor:
+            for entry in self.entries:
+                # Create a new record
+                sql = "INSERT INTO `Spendings_ExtrasDeCont` (`SpreadsheetType`, `Account`, " \
+                      "`Label`, `Name`, `Date`, `Value`) VALUES (%s, %s, %s, %s, %s, %s)"
+                cursor.execute(sql, (entry.statementType, entry.account, entry.label, entry.description, entry.datelog.isoformat(), entry.value))
+        connection.commit()
+
+
+
+
 
 
